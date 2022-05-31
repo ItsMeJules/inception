ac6b3f23fb32d27795e722f9b4fc603207d5da88
Jules <jules.prn06@gmail.com>
2022-05-31 18:51:00 +0200

    Mariadb isnt working yet

---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+service mysql start
+echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql
+echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;" | mysql
+echo "FLUSH PRIVILEGES;" | mysql
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');" | mysql
+service mysql stop
+
+exec mysqld -u mysql
